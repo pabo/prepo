d03e9094e8d6dfb6e3c5d06098c90b5b55cc78cd
Brett Schellenberg <brett.schellenberg@gmail.com>
2016-05-24 16:27:57 -0700

    initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Task;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/task', function (Request $request) {
+	// get all tasks
+	$tasks = Task::orderBy('created_at', 'asc')->get();
+
+	return view('tasks', [
+		'tasks' => $tasks
+	]);
+});
+
+Route::post('/task', function (Request $request) {
+	// Add a new task
+	$validator = Validator::make($request->all(), [
+		'name' => 'required|max:255',
+	]);
+
+	if ($validator->fails()) {
+		return redirect('/task')->withInput()->withErrors($validator);
+	}
+
+	$task = new Task;
+	$task->name = $request->name;
+	$task->save();
+
+	return redirect('/task');
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+	$task->delete();
+
+	return redirect('/task');
+});
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Prepo</title>
+            <!-- Bootstrap CDN -->
+            <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+            <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
